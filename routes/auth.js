2a8df0b0821de96e656f9b2c2baed2bf7a2e7fdd
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-09-01 22:16:26 +1200

    build(server): template engine and static filepath specified
    Basic auth routes also configured, for testing login and
    registration api calls. Index page includes links which
    redirect to login and register pages.

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
 
+router.get('/login', (req, res) =>{
+    res.redirect('/login.html');
+});
+
+router.post('/login', (req, res) => {
+    res.send(req.body);
+});
+
+router.get('/register', (req, res) => {
+    res.redirect('/register.html');
+});
+
+router.post('/register', (req, res) => {
+    res.send(req.body);
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 require('dotenv').config(); // Configure environment variables
 const express = require('express');
 const routes = require('./routes/startup/initRoutes');
+const path = require('path');
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 // Init Routes
 routes(app);
