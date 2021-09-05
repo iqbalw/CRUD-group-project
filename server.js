611ce0ffe12a639e3bf460b75dd5803ea2c80f4d
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-09-06 00:47:46 +1200

    fix: middleware added to parse form data
    The middleware parses the text as URL encoded data which
    is how browsers tend to send form data from regular forms
    set to POST, and exposes the resulting object containing
    keys and values on req.body.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 const app = express();
 
 // Middlewares
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
