c5cac27705b84eca9800a11770a75c967a129a9a
iqbalw <wan.iqbal25@gmail.com>
2021-09-30 08:33:43 +1300

    Add base for #17

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,28 @@
-const router = require('express').Router();
-const {isLoggedIn } = require('../controllers/middleware/verifyUser');
+const router = require("express").Router();
+const Product = require("../model/Products");
+const { isLoggedIn } = require("../controllers/middleware/verifyUser");
 
-// Protected route example/test
-router.get('/', isLoggedIn, (req, res) => {
-    res.send(`${req.user.name} is Logged in`);
+router.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.send(products);
+  } catch (err) {
+    res.status(500).send("No Products retrieved.");
+  }
 });
 
+router.get("/products/:name", async (req, res) => {
+  try {
+    const products = await Product.find({ name: req.params.name });
+    res.send(products);
+  } catch (err) {
+    res.status(400).send("No Products retrieved.");
+  }
+});
+
+// Protected route example/test
+router.get("/", isLoggedIn, (req, res) => {
+  res.send(`${req.user.name} is Logged in`);
+});
 
 module.exports = router;
