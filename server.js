fd96a12fcf5081bb9bb34e99a2d7435891552a9e
Praveen Bandarage <bandarprav@ecs.vuw.ac.nz>
2021-10-16 20:30:02 +1300

    Add client-side testing

--- server.js
+++ server.js
@@ -41,6 +41,31 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+const getDurationInMilliseconds  = (start) => {
+    const NS_PER_SEC = 1e9
+    const NS_TO_MS = 1e6
+    const diff = process.hrtime(start)
+
+    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
+}
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl} [STARTED]`)
+    const start = process.hrtime()
+
+    res.on('finish', () => {            
+        const durationInMilliseconds = getDurationInMilliseconds (start)
+        console.log(`${req.method} ${req.originalUrl} [FINISHED] ${durationInMilliseconds.toLocaleString()} ms`)
+    })
+
+    res.on('close', () => {
+        const durationInMilliseconds = getDurationInMilliseconds (start)
+        console.log(`${req.method} ${req.originalUrl} [CLOSED] ${durationInMilliseconds.toLocaleString()} ms`)
+    })
+
+    next()
+})
+
 // Init Routes
 routes(app);
 
