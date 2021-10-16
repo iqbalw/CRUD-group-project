5726a25f4ac3b23938e4d88f941965af56f44147
Pravin Modotholi <pravinmanoj44@gmail.com>
2021-10-16 17:29:52 +1300

    fix: OAuth redirect url update

--- startup/configPassport.js
+++ startup/configPassport.js
@@ -44,7 +44,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback",
+        callbackURL: process.env.GOOGLE_CALLBACK,
       },
       function (accessToken, refreshToken, profile, done) {
         User.findOrCreate({ 
